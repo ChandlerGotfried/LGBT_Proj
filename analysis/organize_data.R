2eb4b1e083251a468ca5400a3d9fca8c2a229779
Chandler Gotfried <gotfried@uoregon.edu>
2019-05-30 09:23:03 -0700

    added and changed data

--- analysis/organize_data.R
+++ analysis/organize_data.R
@@ -10,30 +10,38 @@
 library(haven)
 SJSD <- read_sav("analysis/input/SJSD.sav")
 
-sjsd <- subset(SJSD, select=c ("q1a","q1c","q4a","q4c","q5b","q6a","q6c","q13a","q14a","q14b",
+sjsd <- subset(SJSD, select=c ("q1a","q1c","q4a","q4c","q5b","q6a","q6c","q8a","q13a","q14a","q14b",
                                "q14c","q14d","q14e","q15a","q18c","q24c","q24d"))
 
 # recode time traveled for LBGT gatherings
-sjsd$time[sjsd$q1c==1] <- "15 minutes"
-sjsd$time[sjsd$q1c==2] <- "30 minutes"
-sjsd$time[sjsd$q1c==3] <- "45 minutes"
-sjsd$time[sjsd$q1c==4] <- "60 minutes"
-sjsd$time[sjsd$q1c==5 | sjsd$q1c==6] <- "90 minutes"
-sjsd$time <- factor(sjsd$time, 
+sjsd$q1c[sjsd$q1c==1] <- "15 minutes"
+sjsd$q1c[sjsd$q1c==2] <- "30 minutes"
+sjsd$q1c[sjsd$q1c==3] <- "45 minutes"
+sjsd$q1c[sjsd$q1c==4] <- "60 minutes"
+sjsd$q1c[sjsd$q1c==5 | sjsd$q1c==6] <- "90 minutes"
+sjsd$q1c <- factor(sjsd$q1c, 
                     levels=c("15 minutes", "30 minutes",
                              "45 minutes", "60 minutes",
                              "90 minutes"))
 
-# recode current sexuality 
+# recode current sexual identity 
 
-sjsd$sex_id[sjsd$q18c==1 | sjsd$q18c==2 | sjsd$q18c==3 | sjsd$q18c==4 |
+sjsd$q18c[sjsd$q18c==1 | sjsd$q18c==2 | sjsd$q18c==3 | sjsd$q18c==4 |
                   sjsd$q18c==5 | sjsd$q18c==6 | sjsd$q18c==7] <- "LGBTQ"
-sjsd$sex_id[sjsd$q18c==8] <- "Heterosexual"
-sjsd$sex_id[sjsd$q18c==9 | sjsd$q18c==10 | sjsd$q18c==11 | sjsd$q18c==12] <- "Nonbinary"
-sjsd$sex_id <- factor(sjsd$sex_id,
+sjsd$q18c[sjsd$q18c==8] <- "Heterosexual"
+sjsd$q18c[sjsd$q18c==9 | sjsd$q18c==10 | sjsd$q18c==11 | sjsd$q18c==12] <- "Nonbinary"
+sjsd$q18c <- factor(sjsd$q18c,
                       levels=c("LGBTQ","Heterosexual","Nonbinary"))
 
 
+# recode how often an event has been attended
 
- 
+sjsd$q8a[sjsd$q8a==1] <- "Never"
+sjsd$q8a[sjsd$q8a==2] <- "Once/Twice a year"
+sjsd$q8a[sjsd$q8a==3] <- "Six times a year"
+sjsd$q8a[sjsd$q8a==4] <- "Once a month"
+sjsd$q8a[sjsd$q8a==5 | sjsd$q8a==6] <- "Once a week or more"
+sjsd$q8a <- factor(sjsd$q8a,
+                   levels=c("Never","Once/Twice a year","Six times a year","Once a month","Once a week or more"))
+
 
