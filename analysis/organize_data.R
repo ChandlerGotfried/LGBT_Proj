6cb838fa75bbcf1755d7d97ed6b82abc3e0c46fc
Chandler Gotfried <gotfried@uoregon.edu>
2019-06-02 12:40:49 -0700

    boxplot trouble

--- analysis/organize_data.R
+++ analysis/organize_data.R
@@ -16,12 +16,17 @@ sjsd <- subset(SJSD, select=c ("q1a","q1c","q4a","q4c","q5b","q6a","q6c","q8a","
                                "q14a","q14b","q14c","q14d","q14e","q15a","q18c","q24c","q24d"))
 
 
-# 
+sjsd$q14a[sjsd$q14a==1] <- "None"
+sjsd$q14a[sjsd$q14a==2] <- "Some"
+sjsd$q14a[sjsd$q14a==3] <- "About half"
+sjsd$q14a[sjsd$q14a==4] <- "Most"
+sjsd$q14a[sjsd$q14a==5] <- "All"
+sjsd$q14a <- factor(sjsd$q14a,
+                    levels=c("None","Some","About half","Most","All"))
 
 
-
-
-
+boxplot(q15a~q14a, data=sjsd, na.rm=TRUE, names=c("None","Some","About half","Most","All"), 
+        xlab="Coming out to family", ylab="Level of upport from family")
 
 
 
