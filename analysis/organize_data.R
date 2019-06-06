e3aae84319f13cb1046cc520517671b1b7c644ec
Chandler Gotfried <gotfried@uoregon.edu>
2019-06-05 23:30:48 -0700

    finished analytical data and have created graphs

--- analysis/organize_data.R
+++ analysis/organize_data.R
@@ -12,38 +12,73 @@ library(haven)
 SJSD <- read_sav("analysis/input/SJSD.sav")
 View(SJSD)
 
-sjsd <- subset(SJSD, select=c ("q1a","q1c","q4a","q4c","q5b","q6a","q6c","q8a","q13a",
+# subset data and omit heterosexual data
+
+sjsd <- subset(SJSD, q18c!=8,
+               select=c ("q1a","q1c","q4a","q4c","q5b","q6a","q6c","q8a","q13a",
                                "q14a","q14b","q14c","q14d","q14e","q15a","q18c","q24c","q24d"))
 
-# recoded coming out to how much of the family
-
-sjsd$q14a[sjsd$q14a==1] <- "None"
-sjsd$q14a[sjsd$q14a==2] <- "Some"
-sjsd$q14a[sjsd$q14a==3] <- "About half"
-sjsd$q14a[sjsd$q14a==4] <- "Most"
-sjsd$q14a[sjsd$q14a==5] <- "All"
-sjsd$q14a <- factor(sjsd$q14a,
+# recoded coming out to how many family members
+sjsd$out_family <- NA
+sjsd$out_family[sjsd$q14a==1] <- "None"
+sjsd$out_family[sjsd$q14a==2] <- "Some"
+sjsd$out_family[sjsd$q14a==3] <- "About half"
+sjsd$out_family[sjsd$q14a==4] <- "Most"
+sjsd$out_family[sjsd$q14a==5] <- "All"
+sjsd$out_family <- factor(sjsd$out_family,
                     levels=c("None","Some","About half","Most","All"))
+table(sjsd$out_family, sjsd$q14a, exclude=NULL)
 
-# boxplot for coming out to family and support from family
+# recode coming out to friends
+sjsd$out_friends <- NA
+sjsd$out_friends[sjsd$q14b==1] <- "None"
+sjsd$out_friends[sjsd$q14b==2] <- "Some"
+sjsd$out_friends[sjsd$q14b==3] <- "About half"
+sjsd$out_friends[sjsd$q14b==4] <- "Most"
+sjsd$out_friends[sjsd$q14b==5] <- "All"
+sjsd$out_friends <- factor(sjsd$out_friends,
+                          levels=c("None","Some","About half","Most","All"))
+table(sjsd$out_friends, sjsd$q14b, exclude=NULL)
 
-boxplot(q15a~q14a, data=sjsd, na.rm=TRUE, names=c("None","Some","About half","Most","All"), 
-        xlab="Coming out to family", ylab="Level of upport from family")
+#recode gay marriage variable
+sjsd$marriage <- NA
+sjsd$marriage[sjsd$q4c==0] <- "False"
+sjsd$marriage[sjsd$q4c==1] <- "True"
+table(sjsd$marriage, sjsd$q4c, exclude=NULL)
 
-count <- table(sjsd$q4a)
-percent <- 100*count/sum(count)
-barplot(percent, main="Gay marriage support", horiz=TRUE, names.arg = c("No", "Yes"),
-        xlab="Percent")
+#recode same sex military variable
+sjsd$military <- NA
+sjsd$military[sjsd$q4a==0] <- "False"
+sjsd$military[sjsd$q4a==1] <- "True"
+table(sjsd$military, sjsd$q4a, exclude=NULL)
 
-counts <- table(sjsd$q4c)
-percent <- 100*counts/sum(counts)
-barplot(percent, main="Support of LGBT in the military", horiz=TRUE, names.arg = c("No", "Yes"),
-        xlab="Percent")
+# change name for coming out to neighbors
+sjsd$out_neighbors <- sjsd$q14e
 
-tab <- table(sjsd$q4c, sjsd$q4a)
-percent <- 100*tab/sum(tab)
-barplot(t(prop.table(percent,1)), beside=TRUE, names.arg=c("Same-sex military","Gay marriage"), 
-        las=1, main="Support gay marriage and\nsame sex military members", ylab="Percent of reponses for support", 
-        col=c("darkgreen", "yellow"), legend.text=c("No","Yes"), cex.names="0.7")
+# change name for family support
+sjsd$fam_support <- sjsd$q15a
+
+# change name for participated in a LGBT event  
+sjsd$event <- sjsd$q8a
+
+# change name for neighborhood homophobia
+sjsd$neigh_phobia <- sjsd$q5b
+
+# combined happiness and enjoyment into 1 variable
+# minus 2 to make the data set start from 0
+sjsd$happiness <- sjsd$q24c+sjsd$q24d-2
+
+# combined bond with community and people into 1 variable
+# minus 2 to make the data set start from 0
+sjsd$lgbt_bond <- sjsd$q6a+sjsd$q6c-2 
+
+
+
+sjsd <- na.omit(subset(sjsd,
+                       select=c("out_family","happiness","out_friends","out_neighbors","fam_support",
+                                "event","lgbt_bond","neigh_phobia","marriage","military")))
+
+save(sjsd, file="analysis/output/analytical_data.RData")
+
 
 
